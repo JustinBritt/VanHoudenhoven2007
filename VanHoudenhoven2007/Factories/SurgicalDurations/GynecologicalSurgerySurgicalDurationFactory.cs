329b80177099d5bda3a90b762bff6b62d0f4bc37
Justin Britt <britt@uwindsor.ca>
2022-03-25 18:13:51 -0400

    Added factory

---
+++ VanHoudenhoven2007/Factories/SurgicalDurations/GynecologicalSurgerySurgicalDurationFactory.cs
@@ -0,0 +1,28 @@
+﻿namespace VanHoudenhoven2007.Factories.SurgicalDurations
+{
+    using VanHoudenhoven2007.Interfaces.SurgicalDurations;
+    using VanHoudenhoven2007.InterfacesFactories.SurgicalDurations;
+    using VanHoudenhoven2007.Records.SurgicalDurations;
+
+    internal sealed class GynecologicalSurgerySurgicalDurationFactory : IGynecologicalSurgerySurgicalDurationFactory
+    {
+        public GynecologicalSurgerySurgicalDurationFactory()
+        {
+        }
+
+        public IGynecologicalSurgerySurgicalDuration Create()
+        {
+            IGynecologicalSurgerySurgicalDuration surgicalDuration = null;
+
+            try
+            {
+                surgicalDuration = new GynecologicalSurgerySurgicalDuration();
+            }
+            finally
+            {
+            }
+
+            return surgicalDuration;
+        }
+    }
+}
