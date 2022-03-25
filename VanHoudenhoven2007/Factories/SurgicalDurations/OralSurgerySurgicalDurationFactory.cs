3fb08e0c044f24cc9ca4c9154c1bbd5956a97d4e
Justin Britt <britt@uwindsor.ca>
2022-03-25 18:29:18 -0400

    Added factory

---
+++ VanHoudenhoven2007/Factories/SurgicalDurations/OralSurgerySurgicalDurationFactory.cs
@@ -0,0 +1,28 @@
+﻿namespace VanHoudenhoven2007.Factories.SurgicalDurations
+{
+    using VanHoudenhoven2007.Interfaces.SurgicalDurations;
+    using VanHoudenhoven2007.InterfacesFactories.SurgicalDurations;
+    using VanHoudenhoven2007.Records.SurgicalDurations;
+
+    internal sealed class OralSurgerySurgicalDurationFactory : IOralSurgerySurgicalDurationFactory
+    {
+        public OralSurgerySurgicalDurationFactory()
+        {
+        }
+
+        public IOralSurgerySurgicalDuration Create()
+        {
+            IOralSurgerySurgicalDuration surgicalDuration = null;
+
+            try
+            {
+                surgicalDuration = new OralSurgerySurgicalDuration();
+            }
+            finally
+            {
+            }
+
+            return surgicalDuration;
+        }
+    }
+}
---
+++ VanHoudenhoven2007/InterfacesFactories/SurgicalDurations/IOralSurgerySurgicalDurationFactory.cs
@@ -0,0 +1,9 @@
+﻿namespace VanHoudenhoven2007.InterfacesFactories.SurgicalDurations
+{
+    using VanHoudenhoven2007.Interfaces.SurgicalDurations;
+
+    public interface IOralSurgerySurgicalDurationFactory
+    {
+        IOralSurgerySurgicalDuration Create();
+    }
+}
