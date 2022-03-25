497e88f10c9e044d134ab307bc3f47a90c8f4ea2
Justin Britt <britt@uwindsor.ca>
2022-03-25 18:38:01 -0400

    Added factory

---
+++ VanHoudenhoven2007/Factories/SurgicalDurations/TraumaSurgicalDurationFactory.cs
@@ -0,0 +1,28 @@
+﻿namespace VanHoudenhoven2007.Factories.SurgicalDurations
+{
+    using VanHoudenhoven2007.Interfaces.SurgicalDurations;
+    using VanHoudenhoven2007.InterfacesFactories.SurgicalDurations;
+    using VanHoudenhoven2007.Records.SurgicalDurations;
+
+    internal sealed class TraumaSurgicalDurationFactory : ITraumaSurgicalDurationFactory
+    {
+        public TraumaSurgicalDurationFactory()
+        {
+        }
+
+        public ITraumaSurgicalDuration Create()
+        {
+            ITraumaSurgicalDuration surgicalDuration = null;
+
+            try
+            {
+                surgicalDuration = new TraumaSurgicalDuration();
+            }
+            finally
+            {
+            }
+
+            return surgicalDuration;
+        }
+    }
+}
