b97596199f4530517dc1de4c8fd1d8ec771b5680
Justin Britt <britt@uwindsor.ca>
2022-03-25 18:46:50 -0400

    Added factory

---
+++ VanHoudenhoven2007/Factories/SurgicalFrequencies/EarNoseThroatSurgerySurgicalFrequencyFactory.cs
@@ -0,0 +1,28 @@
+﻿namespace VanHoudenhoven2007.Factories.SurgicalFrequencies
+{
+    using VanHoudenhoven2007.Interfaces.SurgicalFrequencies;
+    using VanHoudenhoven2007.InterfacesFactories.SurgicalFrequencies;
+    using VanHoudenhoven2007.Records.SurgicalFrequencies;
+
+    internal sealed class EarNoseThroatSurgerySurgicalFrequencyFactory : IEarNoseThroatSurgerySurgicalFrequencyFactory
+    {
+        public EarNoseThroatSurgerySurgicalFrequencyFactory()
+        {
+        }
+
+        public IEarNoseThroatSurgerySurgicalFrequency Create()
+        {
+            IEarNoseThroatSurgerySurgicalFrequency surgicalFrequency = null;
+
+            try
+            {
+                surgicalFrequency = new EarNoseThroatSurgerySurgicalFrequency();
+            }
+            finally
+            {
+            }
+
+            return surgicalFrequency;
+        }
+    }
+}
