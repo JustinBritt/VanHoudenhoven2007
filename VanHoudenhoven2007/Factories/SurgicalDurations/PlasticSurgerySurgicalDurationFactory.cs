c72f2c324dadc47b18ccb6c25d31a57959984286
Justin Britt <britt@uwindsor.ca>
2022-03-25 18:35:49 -0400

    Added factory

---
+++ VanHoudenhoven2007/Factories/SurgicalDurations/PlasticSurgerySurgicalDurationFactory.cs
@@ -0,0 +1,28 @@
+﻿namespace VanHoudenhoven2007.Factories.SurgicalDurations
+{
+    using VanHoudenhoven2007.Interfaces.SurgicalDurations;
+    using VanHoudenhoven2007.InterfacesFactories.SurgicalDurations;
+    using VanHoudenhoven2007.Records.SurgicalDurations;
+
+    internal sealed class PlasticSurgerySurgicalDurationFactory : IPlasticSurgerySurgicalDurationFactory
+    {
+        public PlasticSurgerySurgicalDurationFactory()
+        {
+        }
+
+        public IPlasticSurgerySurgicalDuration Create()
+        {
+            IPlasticSurgerySurgicalDuration surgicalDuration = null;
+
+            try
+            {
+                surgicalDuration = new PlasticSurgerySurgicalDuration();
+            }
+            finally
+            {
+            }
+
+            return surgicalDuration;
+        }
+    }
+}
