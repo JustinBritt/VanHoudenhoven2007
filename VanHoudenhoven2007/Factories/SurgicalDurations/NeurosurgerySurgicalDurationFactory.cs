b41907ae9365cb901d1b692d622dbd5f2937e82a
Justin Britt <britt@uwindsor.ca>
2022-03-25 18:20:36 -0400

    Added factory

---
+++ VanHoudenhoven2007/Factories/SurgicalDurations/NeurosurgerySurgicalDurationFactory.cs
@@ -0,0 +1,28 @@
+﻿namespace VanHoudenhoven2007.Factories.SurgicalDurations
+{
+    using VanHoudenhoven2007.Interfaces.SurgicalDurations;
+    using VanHoudenhoven2007.InterfacesFactories.SurgicalDurations;
+    using VanHoudenhoven2007.Records.SurgicalDurations;
+
+    internal sealed class NeurosurgerySurgicalDurationFactory : INeurosurgerySurgicalDurationFactory
+    {
+        public NeurosurgerySurgicalDurationFactory()
+        {
+        }
+
+        public INeurosurgerySurgicalDuration Create()
+        {
+            INeurosurgerySurgicalDuration surgicalDuration = null;
+
+            try
+            {
+                surgicalDuration = new NeurosurgerySurgicalDuration();
+            }
+            finally
+            {
+            }
+
+            return surgicalDuration;
+        }
+    }
+}
