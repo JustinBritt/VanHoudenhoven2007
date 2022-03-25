f7c9f81629d40bcc2682c10867046bd96b93284d
Justin Britt <britt@uwindsor.ca>
2022-03-25 18:24:45 -0400

    Added factory

---
+++ VanHoudenhoven2007/Factories/SurgicalDurations/OphthalmologySurgicalDurationFactory.cs
@@ -0,0 +1,28 @@
+﻿namespace VanHoudenhoven2007.Factories.SurgicalDurations
+{
+    using VanHoudenhoven2007.Interfaces.SurgicalDurations;
+    using VanHoudenhoven2007.InterfacesFactories.SurgicalDurations;
+    using VanHoudenhoven2007.Records.SurgicalDurations;
+
+    internal sealed class OphthalmologySurgicalDurationFactory : IOphthalmologySurgicalDurationFactory
+    {
+        public OphthalmologySurgicalDurationFactory()
+        {
+        }
+
+        public IOphthalmologySurgicalDuration Create()
+        {
+            IOphthalmologySurgicalDuration surgicalDuration = null;
+
+            try
+            {
+                surgicalDuration = new OphthalmologySurgicalDuration();
+            }
+            finally
+            {
+            }
+
+            return surgicalDuration;
+        }
+    }
+}
