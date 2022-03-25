ed9226d9ddd756c4fa300ef3a39af22ad37b57ca
Justin Britt <britt@uwindsor.ca>
2022-03-25 06:52:21 -0400

    Added factory

---
+++ VanHoudenhoven2007/Factories/SurgicalDurations/EarNoseThroatSurgerySurgicalDurationFactory.cs
@@ -0,0 +1,28 @@
+﻿namespace VanHoudenhoven2007.Factories.SurgicalDurations
+{
+    using VanHoudenhoven2007.Interfaces.SurgicalDurations;
+    using VanHoudenhoven2007.InterfacesFactories.SurgicalDurations;
+    using VanHoudenhoven2007.Records.SurgicalDurations;
+
+    internal sealed class EarNoseThroatSurgerySurgicalDurationFactory : IEarNoseThroatSurgerySurgicalDurationFactory
+    {
+        public EarNoseThroatSurgerySurgicalDurationFactory()
+        {
+        }
+
+        public IEarNoseThroatSurgerySurgicalDuration Create()
+        {
+            IEarNoseThroatSurgerySurgicalDuration surgicalDuration = null;
+
+            try
+            {
+                surgicalDuration = new EarNoseThroatSurgerySurgicalDuration();
+            }
+            finally
+            {
+            }
+
+            return surgicalDuration;
+        }
+    }
+}
