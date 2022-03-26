b8dcf79c8dd5308a16af80c98c1689ca53920f6d
Justin Britt <britt@uwindsor.ca>
2022-03-26 11:25:28 -0400

    Added factory

---
+++ VanHoudenhoven2007/Factories/Dependencies/Hl7.Fhir.R4/Model/NullableValueFactory.cs
@@ -0,0 +1,48 @@
+﻿namespace VanHoudenhoven2007.Factories.Dependencies.Hl7.Fhir.R4.Model
+{
+    using System;
+
+    using global::Hl7.Fhir.Model;
+
+    using VanHoudenhoven2007.InterfacesFactories.Dependencies.Hl7.Fhir.R4.Model;
+
+    internal sealed class NullableValueFactory : INullableValueFactory
+    {
+        public NullableValueFactory()
+        {
+        }
+
+        public INullableValue<T> Create<T>(
+            T? value)
+            where T : struct
+        {
+            INullableValue<T> instance = null;
+
+            try
+            {
+                PrimitiveType primitiveType = value switch
+                {
+                    bool b => new FhirBoolean(
+                        b),
+
+                    decimal d => new FhirDecimal(
+                        d),
+
+                    int i => new PositiveInt(
+                        i),
+
+                    { } => throw new ArgumentNullException(nameof(value)),
+
+                    _ => null
+                };
+
+                instance = (INullableValue<T>)primitiveType;
+            }
+            finally
+            {
+            }
+
+            return instance;
+        }
+    }
+}
