c98cdfb68daae10eed822a07bce4075af8a167ad
Justin Britt <britt@uwindsor.ca>
2022-03-26 09:00:12 -0400

    Added factory

---
+++ VanHoudenhoven2007/Factories/SurgicalFrequencies/NeurosurgerySurgicalFrequencyFactory.cs
@@ -0,0 +1,28 @@
+﻿namespace VanHoudenhoven2007.Factories.SurgicalFrequencies
+{
+    using VanHoudenhoven2007.Interfaces.SurgicalFrequencies;
+    using VanHoudenhoven2007.InterfacesFactories.SurgicalFrequencies;
+    using VanHoudenhoven2007.Records.SurgicalFrequencies;
+
+    internal sealed class NeurosurgerySurgicalFrequencyFactory : INeurosurgerySurgicalFrequencyFactory
+    {
+        public NeurosurgerySurgicalFrequencyFactory()
+        {
+        }
+
+        public INeurosurgerySurgicalFrequency Create()
+        {
+            INeurosurgerySurgicalFrequency surgicalFrequency = null;
+
+            try
+            {
+                surgicalFrequency = new NeurosurgerySurgicalFrequency();
+            }
+            finally
+            {
+            }
+
+            return surgicalFrequency;
+        }
+    }
+}
---
+++ VanHoudenhoven2007/InterfacesFactories/SurgicalFrequencies/INeurosurgerySurgicalFrequencyFactory.cs
@@ -0,0 +1,9 @@
+﻿namespace VanHoudenhoven2007.InterfacesFactories.SurgicalFrequencies
+{
+    using VanHoudenhoven2007.Interfaces.SurgicalFrequencies;
+
+    public interface INeurosurgerySurgicalFrequencyFactory
+    {
+        INeurosurgerySurgicalFrequency Create();
+    }
+}
