7c5a182ddba063e6833158d1672a024d4db900d7
Justin Britt <britt@uwindsor.ca>
2022-03-26 11:38:48 -0400

    Added abstract factory

---
+++ VanHoudenhoven2007/AbstractFactories/DependenciesAbstractFactory.cs
@@ -0,0 +1,43 @@
+﻿namespace VanHoudenhoven2007.AbstractFactories
+{
+    using VanHoudenhoven2007.Factories.Dependencies.Hl7.Fhir.R4.Model;
+    using VanHoudenhoven2007.InterfacesAbstractFactories;
+    using VanHoudenhoven2007.InterfacesFactories.Dependencies.Hl7.Fhir.R4.Model;
+
+    internal sealed class DependenciesAbstractFactory : IDependenciesAbstractFactory
+    {
+        public DependenciesAbstractFactory()
+        {
+        }
+
+        public IDurationFactory CreateDurationFactory()
+        {
+            IDurationFactory factory = null;
+
+            try
+            {
+                factory = new DurationFactory();
+            }
+            finally
+            {
+            }
+
+            return factory;
+        }
+
+        public INullableValueFactory CreateNullableValueFactory()
+        {
+            INullableValueFactory factory = null;
+
+            try
+            {
+                factory = new NullableValueFactory();
+            }
+            finally
+            {
+            }
+
+            return factory;
+        }
+    }
+}
---
+++ VanHoudenhoven2007/InterfacesAbstractFactories/IDependenciesAbstractFactory.cs
@@ -0,0 +1,11 @@
+﻿namespace VanHoudenhoven2007.InterfacesAbstractFactories
+{
+    using VanHoudenhoven2007.InterfacesFactories.Dependencies.Hl7.Fhir.R4.Model;
+
+    public interface IDependenciesAbstractFactory
+    {
+        IDurationFactory CreateDurationFactory();
+
+        INullableValueFactory CreateNullableValueFactory();
+    }
+}
