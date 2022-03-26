46ffe182850251b42b91ea9cd89c8b8e551eaa9f
Justin Britt <britt@uwindsor.ca>
2022-03-26 11:31:55 -0400

    Added factory

---
+++ VanHoudenhoven2007/Factories/Dependencies/Hl7.Fhir.R4/Model/DurationFactory.cs
@@ -0,0 +1,63 @@
+﻿namespace VanHoudenhoven2007.Factories.Dependencies.Hl7.Fhir.R4.Model
+{
+    using global::Hl7.Fhir.Model;
+
+    using VanHoudenhoven2007.InterfacesFactories.Dependencies.Hl7.Fhir.R4.Model;
+
+    internal sealed class DurationFactory : IDurationFactory
+    {
+        private const string unitsofmeasure = "http://unitsofmeasure.org";
+
+        public DurationFactory()
+        {
+        }
+
+        public Duration Create()
+        {
+            Duration duration;
+
+            try
+            {
+                duration = new Duration();
+            }
+            finally
+            {
+            }
+
+            return duration;
+        }
+
+        public Duration Create(
+            string unit,
+            decimal value)
+        {
+            return this.Create(
+                unitsofmeasure,
+                unit,
+                value);
+        }
+
+        public Duration Create(
+            string system,
+            string unit,
+            decimal value)
+        {
+            Duration duration;
+
+            try
+            {
+                duration = new Duration()
+                {
+                    System = system,
+                    Unit = unit,
+                    Value = value
+                };
+            }
+            finally
+            {
+            }
+
+            return duration;
+        }
+    }
+}
