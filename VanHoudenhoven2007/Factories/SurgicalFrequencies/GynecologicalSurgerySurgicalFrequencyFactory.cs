973b99a9a11b76c13618de1ead84ee2d1f8d60fb
Justin Britt <britt@uwindsor.ca>
2022-03-26 08:55:35 -0400

    Added factory

---
+++ VanHoudenhoven2007/Factories/SurgicalFrequencies/GynecologicalSurgerySurgicalFrequencyFactory.cs
@@ -0,0 +1,28 @@
+﻿namespace VanHoudenhoven2007.Factories.SurgicalFrequencies
+{
+    using VanHoudenhoven2007.Interfaces.SurgicalFrequencies;
+    using VanHoudenhoven2007.InterfacesFactories.SurgicalFrequencies;
+    using VanHoudenhoven2007.Records.SurgicalFrequencies;
+
+    internal sealed class GynecologicalSurgerySurgicalFrequencyFactory : IGynecologicalSurgerySurgicalFrequencyFactory
+    {
+        public GynecologicalSurgerySurgicalFrequencyFactory()
+        {
+        }
+
+        public IGynecologicalSurgerySurgicalFrequency Create()
+        {
+            IGynecologicalSurgerySurgicalFrequency surgicalFrequency = null;
+
+            try
+            {
+                surgicalFrequency = new GynecologicalSurgerySurgicalFrequency();
+            }
+            finally
+            {
+            }
+
+            return surgicalFrequency;
+        }
+    }
+}
