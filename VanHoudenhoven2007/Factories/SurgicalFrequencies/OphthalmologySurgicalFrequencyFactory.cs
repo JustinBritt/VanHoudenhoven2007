28c2aa47cd966ac92334bfc22f8a30230649c007
Justin Britt <britt@uwindsor.ca>
2022-03-26 09:22:41 -0400

    Added factory

---
+++ VanHoudenhoven2007/Factories/SurgicalFrequencies/OphthalmologySurgicalFrequencyFactory.cs
@@ -0,0 +1,28 @@
+﻿namespace VanHoudenhoven2007.Factories.SurgicalFrequencies
+{
+    using VanHoudenhoven2007.Interfaces.SurgicalFrequencies;
+    using VanHoudenhoven2007.InterfacesFactories.SurgicalFrequencies;
+    using VanHoudenhoven2007.Records.SurgicalFrequencies;
+
+    internal sealed class OphthalmologySurgicalFrequencyFactory : IOphthalmologySurgicalFrequencyFactory
+    {
+        public OphthalmologySurgicalFrequencyFactory()
+        {
+        }
+
+        public IOphthalmologySurgicalFrequency Create()
+        {
+            IOphthalmologySurgicalFrequency surgicalFrequency = null;
+
+            try
+            {
+                surgicalFrequency = new OphthalmologySurgicalFrequency();
+            }
+            finally
+            {
+            }
+
+            return surgicalFrequency;
+        }
+    }
+}
