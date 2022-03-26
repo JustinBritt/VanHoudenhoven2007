b1c59ea703086f7a2794320fc336005cf5fd0dcb
Justin Britt <britt@uwindsor.ca>
2022-03-26 10:09:46 -0400

    Added factory

---
+++ VanHoudenhoven2007/Factories/SurgicalFrequencies/TraumaSurgicalFrequencyFactory.cs
@@ -0,0 +1,28 @@
+﻿namespace VanHoudenhoven2007.Factories.SurgicalFrequencies
+{
+    using VanHoudenhoven2007.Interfaces.SurgicalFrequencies;
+    using VanHoudenhoven2007.InterfacesFactories.SurgicalFrequencies;
+    using VanHoudenhoven2007.Records.SurgicalFrequencies;
+
+    internal sealed class TraumaSurgicalFrequencyFactory : ITraumaSurgicalFrequencyFactory
+    {
+        public TraumaSurgicalFrequencyFactory()
+        {
+        }
+
+        public ITraumaSurgicalFrequency Create()
+        {
+            ITraumaSurgicalFrequency surgicalFrequency = null;
+
+            try
+            {
+                surgicalFrequency = new TraumaSurgicalFrequency();
+            }
+            finally
+            {
+            }
+
+            return surgicalFrequency;
+        }
+    }
+}
