a47ae48ef599a6d9cada996d70cf3c63ba29eec1
Justin Britt <britt@uwindsor.ca>
2022-03-26 12:06:06 -0400

    Added factory

--- VanHoudenhoven2007/Factories/SurgicalDurations/GeneralSurgerySurgicalDurationFactory.cs
+++ VanHoudenhoven2007/Factories/SurgicalDurations/GeneralSurgerySurgicalDurationFactory.cs
@@ -1,6 +1,7 @@
 ﻿namespace VanHoudenhoven2007.Factories.SurgicalDurations
 {
     using VanHoudenhoven2007.Interfaces.SurgicalDurations;
+    using VanHoudenhoven2007.InterfacesFactories.Dependencies.Hl7.Fhir.R4.Model;
     using VanHoudenhoven2007.InterfacesFactories.SurgicalDurations;
     using VanHoudenhoven2007.Records.SurgicalDurations;
 
@@ -10,13 +11,15 @@
         {
         }
 
-        public IGeneralSurgerySurgicalDuration Create()
+        public IGeneralSurgerySurgicalDuration Create(
+            IDurationFactory durationFactory)
         {
             IGeneralSurgerySurgicalDuration surgicalDuration = null;
 
             try
             {
-                surgicalDuration = new GeneralSurgerySurgicalDuration();
+                surgicalDuration = new GeneralSurgerySurgicalDuration(
+                    durationFactory);
             }
             finally
             {
