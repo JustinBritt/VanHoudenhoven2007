4bb171e134403b7ada54f4675740b7ad52e68303
Justin Britt <britt@uwindsor.ca>
2022-03-26 11:16:48 -0400

    Added enum

---
+++ VanHoudenhoven2007/Enums/Category.cs
@@ -0,0 +1,14 @@
+﻿namespace VanHoudenhoven2007.Enums
+{
+    public enum Category
+    {
+        One,
+        Two,
+        Three,
+        Four,
+        Five,
+        Six,
+        Seven,
+        Eight
+    }
+}
