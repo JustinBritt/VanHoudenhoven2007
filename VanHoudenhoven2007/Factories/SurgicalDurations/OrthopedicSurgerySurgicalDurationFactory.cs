9f552ded2b3ceae6aff94f4e01abb9f9a858e469
Justin Britt <britt@uwindsor.ca>
2022-03-27 03:58:15 -0400

    Use factory

--- VanHoudenhoven2007/Factories/SurgicalDurations/OrthopedicSurgerySurgicalDurationFactory.cs
+++ VanHoudenhoven2007/Factories/SurgicalDurations/OrthopedicSurgerySurgicalDurationFactory.cs
@@ -1,6 +1,7 @@
 ﻿namespace VanHoudenhoven2007.Factories.SurgicalDurations
 {
     using VanHoudenhoven2007.Interfaces.SurgicalDurations;
+    using VanHoudenhoven2007.InterfacesFactories.Dependencies.Hl7.Fhir.R4.Model;
     using VanHoudenhoven2007.InterfacesFactories.SurgicalDurations;
     using VanHoudenhoven2007.Records.SurgicalDurations;
 
@@ -10,13 +11,15 @@
         {
         }
 
-        public IOrthopedicSurgerySurgicalDuration Create()
+        public IOrthopedicSurgerySurgicalDuration Create(
+            IDurationFactory durationFactory)
         {
             IOrthopedicSurgerySurgicalDuration surgicalDuration = null;
 
             try
             {
-                surgicalDuration = new OrthopedicSurgerySurgicalDuration();
+                surgicalDuration = new OrthopedicSurgerySurgicalDuration(
+                    durationFactory);
             }
             finally
             {
