cceb44d50461a5671fb0b819bf2abb505de40803
Justin Britt <britt@uwindsor.ca>
2022-04-01 07:54:43 -0400

    Added factory

--- VanHoudenhoven2007/Factories/SurgicalFrequencies/OrthopedicSurgerySurgicalFrequencyFactory.cs
+++ VanHoudenhoven2007/Factories/SurgicalFrequencies/OrthopedicSurgerySurgicalFrequencyFactory.cs
@@ -1,6 +1,7 @@
 ﻿namespace VanHoudenhoven2007.Factories.SurgicalFrequencies
 {
     using VanHoudenhoven2007.Interfaces.SurgicalFrequencies;
+    using VanHoudenhoven2007.InterfacesFactories.Dependencies.Hl7.Fhir.R4.Model;
     using VanHoudenhoven2007.InterfacesFactories.SurgicalFrequencies;
     using VanHoudenhoven2007.Records.SurgicalFrequencies;
 
@@ -10,13 +11,15 @@
         {
         }
 
-        public IOrthopedicSurgerySurgicalFrequency Create()
+        public IOrthopedicSurgerySurgicalFrequency Create(
+            INullableValueFactory nullableValueFactory)
         {
             IOrthopedicSurgerySurgicalFrequency surgicalFrequency = null;
 
             try
             {
-                surgicalFrequency = new OrthopedicSurgerySurgicalFrequency();
+                surgicalFrequency = new OrthopedicSurgerySurgicalFrequency(
+                    nullableValueFactory);
             }
             finally
             {
--- VanHoudenhoven2007/InterfacesFactories/SurgicalFrequencies/IOrthopedicSurgerySurgicalFrequencyFactory.cs
+++ VanHoudenhoven2007/InterfacesFactories/SurgicalFrequencies/IOrthopedicSurgerySurgicalFrequencyFactory.cs
@@ -1,9 +1,11 @@
 ﻿namespace VanHoudenhoven2007.InterfacesFactories.SurgicalFrequencies
 {
     using VanHoudenhoven2007.Interfaces.SurgicalFrequencies;
+    using VanHoudenhoven2007.InterfacesFactories.Dependencies.Hl7.Fhir.R4.Model;
 
     public interface IOrthopedicSurgerySurgicalFrequencyFactory
     {
-        IOrthopedicSurgerySurgicalFrequency Create();
+        IOrthopedicSurgerySurgicalFrequency Create(
+            INullableValueFactory nullableValueFactory);
     }
 }
