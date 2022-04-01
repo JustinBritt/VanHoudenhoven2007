a44daeae6d1c14bdb823a1ac035b5652dcf69dd6
Justin Britt <britt@uwindsor.ca>
2022-04-01 08:48:50 -0400

    Added enum

---
+++ VanHoudenhoven2007/Enums/Specialty.cs
@@ -0,0 +1,16 @@
+﻿namespace VanHoudenhoven2007.Enums
+{
+    public enum Specialty
+    {
+        EarNoseThroatSurgery,
+        GeneralSurgery,
+        GynecologicalSurgery,
+        Neurosurgery,
+        Ophthalmology,
+        OralSurgery,
+        OrthopedicSurgery,
+        PlasticSurgery,
+        Trauma,
+        Urology
+    }
+}
