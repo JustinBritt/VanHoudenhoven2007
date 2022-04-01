93d3f0026843e349cf1206f2e2684ebe4d74cb14
Justin Britt <britt@uwindsor.ca>
2022-04-01 08:00:35 -0400

    Added factory

--- VanHoudenhoven2007/Records/SurgicalFrequencies/PlasticSurgerySurgicalFrequency.cs
+++ VanHoudenhoven2007/Records/SurgicalFrequencies/PlasticSurgerySurgicalFrequency.cs
@@ -3,10 +3,12 @@
     using Hl7.Fhir.Model;
 
     using VanHoudenhoven2007.Interfaces.SurgicalFrequencies;
+    using VanHoudenhoven2007.InterfacesFactories.Dependencies.Hl7.Fhir.R4.Model;
 
     public sealed record PlasticSurgerySurgicalFrequency : IPlasticSurgerySurgicalFrequency
     {
-        public PlasticSurgerySurgicalFrequency()
+        public PlasticSurgerySurgicalFrequency(
+            INullableValueFactory nullableValueFactory)
         {
             this.Category1Frequency = new FhirDecimal((decimal)0.05);
 
