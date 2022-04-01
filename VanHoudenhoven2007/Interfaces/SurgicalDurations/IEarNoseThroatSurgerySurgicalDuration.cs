e07f4a560ce443f4875ee7380b724d2163fd34de
Justin Britt <britt@uwindsor.ca>
2022-04-01 16:36:37 -0400

    Implement interface

--- VanHoudenhoven2007/Interfaces/SurgicalDurations/IEarNoseThroatSurgerySurgicalDuration.cs
+++ VanHoudenhoven2007/Interfaces/SurgicalDurations/IEarNoseThroatSurgerySurgicalDuration.cs
@@ -2,38 +2,7 @@
 {
     using Hl7.Fhir.Model;
 
-    public interface IEarNoseThroatSurgerySurgicalDuration
+    public interface IEarNoseThroatSurgerySurgicalDuration : ISurgicalDuration
     {
-        Duration Category1Mean { get; }
-
-        Duration Category2Mean { get; }
-
-        Duration Category3Mean { get; }
-
-        Duration Category4Mean { get; }
-
-        Duration Category5Mean { get; }
-
-        Duration Category6Mean { get; }
-
-        Duration Category7Mean { get; }
-
-        Duration Category8Mean { get; }
-
-        Duration Category1StandardDeviation { get; }
-
-        Duration Category2StandardDeviation { get; }
-
-        Duration Category3StandardDeviation { get; }
-
-        Duration Category4StandardDeviation { get; }
-
-        Duration Category5StandardDeviation { get; }
-
-        Duration Category6StandardDeviation { get; }
-
-        Duration Category7StandardDeviation { get; }
-
-        Duration Category8StandardDeviation { get; }
     }
 }
