d350d709ca02067274e47fc8aa574599e880994a
Justin Britt <britt@uwindsor.ca>
2022-04-02 13:06:32 -0400

    Added class

---
+++ VanHoudenhoven2007/Classes/Contexts/SurgicalFrequencies/SurgicalFrequencyInputContext.cs
@@ -0,0 +1,22 @@
+﻿namespace VanHoudenhoven2007.Classes.Contexts.SurgicalFrequencies
+{
+    using Hl7.Fhir.Model;
+
+    using VanHoudenhoven2007.Interfaces.Contexts.SurgicalFrequencies;
+
+    internal sealed class SurgicalFrequencyInputContext : ISurgicalFrequencyInputContext
+    {
+        public SurgicalFrequencyInputContext(
+            PositiveInt category,
+            CodeableConcept specialty)
+        {
+            this.Category = category;
+
+            this.Specialty = specialty;
+        }
+
+        public PositiveInt Category { get; }
+
+        public CodeableConcept Specialty { get; }
+    }
+}
---
+++ VanHoudenhoven2007/Interfaces/Contexts/SurgicalFrequencies/ISurgicalFrequencyInputContext.cs
@@ -0,0 +1,11 @@
+﻿namespace VanHoudenhoven2007.Interfaces.Contexts.SurgicalFrequencies
+{
+    using Hl7.Fhir.Model;
+
+    public interface ISurgicalFrequencyInputContext
+    {
+        PositiveInt Category { get; }
+
+        CodeableConcept Specialty { get; }
+    }
+}
