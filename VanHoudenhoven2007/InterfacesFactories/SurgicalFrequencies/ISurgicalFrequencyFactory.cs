12c03d1c6a07be9b0401e7fb093bf5c79e8b58a6
Justin Britt <britt@uwindsor.ca>
2022-04-02 11:01:22 -0400

    Added interface

---
+++ VanHoudenhoven2007/InterfacesFactories/SurgicalFrequencies/ISurgicalFrequencyFactory.cs
@@ -0,0 +1,11 @@
+﻿namespace VanHoudenhoven2007.InterfacesFactories.SurgicalFrequencies
+{
+    using VanHoudenhoven2007.Interfaces.SurgicalFrequencies;
+    using VanHoudenhoven2007.InterfacesFactories.Dependencies.Hl7.Fhir.R4.Model;
+
+    public interface ISurgicalFrequencyFactory
+    {
+        ISurgicalFrequency Create(
+            INullableValueFactory nullableValueFactory);
+    }
+}
