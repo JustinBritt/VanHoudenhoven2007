29de0e93615f19081fb4a95b9657d79b06f034bc
Justin Britt <britt@uwindsor.ca>
2022-04-02 15:30:39 -0400

    Added factory

---
+++ VanHoudenhoven2007/Factories/Contexts/SurgicalDurations/SurgicalDurationOutputContextFactory.cs
@@ -0,0 +1,32 @@
+﻿namespace VanHoudenhoven2007.Factories.Contexts.SurgicalDurations
+{
+    using Hl7.Fhir.Model;
+
+    using VanHoudenhoven2007.Classes.Contexts.SurgicalDurations;
+    using VanHoudenhoven2007.Interfaces.Contexts.SurgicalDurations;
+    using VanHoudenhoven2007.InterfacesFactories.Contexts.SurgicalDurations;
+
+    internal sealed class SurgicalDurationOutputContextFactory : ISurgicalDurationOutputContextFactory
+    {
+        public SurgicalDurationOutputContextFactory()
+        {
+        }
+
+        public ISurgicalDurationOutputContext Create(
+            Duration duration)
+        {
+            ISurgicalDurationOutputContext context = null;
+
+            try
+            {
+                context = new SurgicalDurationOutputContext(
+                    duration);
+            }
+            finally
+            {
+            }
+
+            return context;
+        }
+    }
+}
---
+++ VanHoudenhoven2007/InterfacesFactories/Contexts/SurgicalDurations/ISurgicalDurationOutputContextFactory.cs
@@ -0,0 +1,12 @@
+﻿namespace VanHoudenhoven2007.InterfacesFactories.Contexts.SurgicalDurations
+{
+    using Hl7.Fhir.Model;
+
+    using VanHoudenhoven2007.Interfaces.Contexts.SurgicalDurations;
+
+    public interface ISurgicalDurationOutputContextFactory
+    {
+        ISurgicalDurationOutputContext Create(
+            Duration duration);
+    }
+}
