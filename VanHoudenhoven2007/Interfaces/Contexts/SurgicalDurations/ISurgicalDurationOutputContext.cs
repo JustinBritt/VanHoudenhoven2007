ee057529a490a8c7e2738204b47ba26b9097d392
Justin Britt <britt@uwindsor.ca>
2022-04-02 14:54:48 -0400

    Added class

---
+++ VanHoudenhoven2007/Interfaces/Contexts/SurgicalDurations/ISurgicalDurationOutputContext.cs
@@ -0,0 +1,9 @@
+﻿namespace VanHoudenhoven2007.Interfaces.Contexts.SurgicalDurations
+{
+    using Hl7.Fhir.Model;
+
+    public interface ISurgicalDurationOutputContext
+    {
+        Duration Duration { get; }
+    }
+}
