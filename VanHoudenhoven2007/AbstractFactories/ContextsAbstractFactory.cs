baeaaa6cc83e9c1ae2612071fa9a94670aa54d00
Justin Britt <britt@uwindsor.ca>
2022-04-02 15:39:38 -0400

    Added abstract factory

---
+++ VanHoudenhoven2007/AbstractFactories/ContextsAbstractFactory.cs
@@ -0,0 +1,75 @@
+﻿namespace VanHoudenhoven2007.AbstractFactories
+{
+    using VanHoudenhoven2007.Factories.Contexts.SurgicalDurations;
+    using VanHoudenhoven2007.Factories.Contexts.SurgicalFrequencies;
+    using VanHoudenhoven2007.InterfacesAbstractFactories;
+    using VanHoudenhoven2007.InterfacesFactories.Contexts.SurgicalDurations;
+    using VanHoudenhoven2007.InterfacesFactories.Contexts.SurgicalFrequencies;
+
+    internal sealed class ContextsAbstractFactory : IContextsAbstractFactory
+    {
+        public ContextsAbstractFactory()
+        {
+        }
+
+        public ISurgicalDurationInputContextFactory CreateSurgicalDurationInputContextFactory()
+        {
+            ISurgicalDurationInputContextFactory factory = null;
+
+            try
+            {
+                factory = new SurgicalDurationInputContextFactory();
+            }
+            finally
+            {
+            }
+
+            return factory;
+        }
+
+        public ISurgicalDurationOutputContextFactory CreateSurgicalDurationOutputContextFactory()
+        {
+            ISurgicalDurationOutputContextFactory factory = null;
+
+            try
+            {
+                factory = new SurgicalDurationOutputContextFactory();
+            }
+            finally
+            {
+            }
+
+            return factory;
+        }
+
+        public ISurgicalFrequencyInputContextFactory CreateSurgicalFrequencyInputContextFactory()
+        {
+            ISurgicalFrequencyInputContextFactory factory = null;
+
+            try
+            {
+                factory = new SurgicalFrequencyInputContextFactory();
+            }
+            finally
+            {
+            }
+
+            return factory;
+        }
+
+        public ISurgicalFrequencyOutputContextFactory CreateSurgicalFrequencyOutputContextFactory()
+        {
+            ISurgicalFrequencyOutputContextFactory factory = null;
+
+            try
+            {
+                factory = new SurgicalFrequencyOutputContextFactory();
+            }
+            finally
+            {
+            }
+
+            return factory;
+        }
+    }
+}
---
+++ VanHoudenhoven2007/InterfacesAbstractFactories/IContextsAbstractFactory.cs
@@ -0,0 +1,16 @@
+﻿namespace VanHoudenhoven2007.InterfacesAbstractFactories
+{
+    using VanHoudenhoven2007.InterfacesFactories.Contexts.SurgicalDurations;
+    using VanHoudenhoven2007.InterfacesFactories.Contexts.SurgicalFrequencies;
+
+    public interface IContextsAbstractFactory
+    {
+        ISurgicalDurationInputContextFactory CreateSurgicalDurationInputContextFactory();
+
+        ISurgicalDurationOutputContextFactory CreateSurgicalDurationOutputContextFactory();
+
+        ISurgicalFrequencyInputContextFactory CreateSurgicalFrequencyInputContextFactory();
+
+        ISurgicalFrequencyOutputContextFactory CreateSurgicalFrequencyOutputContextFactory();
+    }
+}
