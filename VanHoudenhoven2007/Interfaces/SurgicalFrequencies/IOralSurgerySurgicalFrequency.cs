02fd73df11fd030e21ce6f609cea1da58cc3b0c3
Justin Britt <britt@uwindsor.ca>
2022-04-02 09:24:48 -0400

    Implement interface

--- VanHoudenhoven2007/Interfaces/SurgicalFrequencies/IOralSurgerySurgicalFrequency.cs
+++ VanHoudenhoven2007/Interfaces/SurgicalFrequencies/IOralSurgerySurgicalFrequency.cs
@@ -1,23 +1,6 @@
 ﻿namespace VanHoudenhoven2007.Interfaces.SurgicalFrequencies
 {
-    using Hl7.Fhir.Model;
-
-    public interface IOralSurgerySurgicalFrequency
+    public interface IOralSurgerySurgicalFrequency : ISurgicalFrequency
     {
-        FhirDecimal Category1Frequency { get; }
-
-        FhirDecimal Category2Frequency { get; }
-
-        FhirDecimal Category3Frequency { get; }
-
-        FhirDecimal Category4Frequency { get; }
-
-        FhirDecimal Category5Frequency { get; }
-
-        FhirDecimal Category6Frequency { get; }
-
-        FhirDecimal Category7Frequency { get; }
-
-        FhirDecimal Category8Frequency { get; }
     }
 }
