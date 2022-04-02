08c6b9b6832015bd026e6d2aa53548df9001b052
Justin Britt <britt@uwindsor.ca>
2022-04-02 15:49:09 -0400

    Added factory

---
+++ VanHoudenhoven2007/Factories/Exports/SurgicalDurations/SurgicalDurationExportFactory.cs
@@ -0,0 +1,28 @@
+﻿namespace VanHoudenhoven2007.Factories.Exports.SurgicalDurations
+{
+    using VanHoudenhoven2007.Classes.Exports.SurgicalDurations;
+    using VanHoudenhoven2007.Interfaces.Exports.SurgicalDurations;
+    using VanHoudenhoven2007.InterfacesFactories.Exports.SurgicalDurations;
+
+    internal sealed class SurgicalDurationExportFactory : ISurgicalDurationExportFactory
+    {
+        public SurgicalDurationExportFactory()
+        {
+        }
+
+        public ISurgicalDurationExport Create()
+        {
+            ISurgicalDurationExport export = null;
+
+            try
+            {
+                export = new SurgicalDurationExport();
+            }
+            finally
+            {
+            }
+
+            return export;
+        }
+    }
+}
---
+++ VanHoudenhoven2007/InterfacesFactories/Exports/SurgicalDurations/ISurgicalDurationExportFactory.cs
@@ -0,0 +1,9 @@
+﻿namespace VanHoudenhoven2007.InterfacesFactories.Exports.SurgicalDurations
+{
+    using VanHoudenhoven2007.Interfaces.Exports.SurgicalDurations;
+
+    public interface ISurgicalDurationExportFactory
+    {
+        ISurgicalDurationExport Create();
+    }
+}
