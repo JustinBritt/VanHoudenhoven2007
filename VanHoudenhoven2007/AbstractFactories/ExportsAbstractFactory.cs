446074156721f141509cbe37d3ffee797a61ad5c
Justin Britt <britt@uwindsor.ca>
2022-04-02 15:56:18 -0400

    Added abstract factory

---
+++ VanHoudenhoven2007/AbstractFactories/ExportsAbstractFactory.cs
@@ -0,0 +1,45 @@
+﻿namespace VanHoudenhoven2007.AbstractFactories
+{
+    using VanHoudenhoven2007.Factories.Exports.SurgicalDurations;
+    using VanHoudenhoven2007.Factories.Exports.SurgicalFrequencies;
+    using VanHoudenhoven2007.InterfacesAbstractFactories;
+    using VanHoudenhoven2007.InterfacesFactories.Exports.SurgicalDurations;
+    using VanHoudenhoven2007.InterfacesFactories.Exports.SurgicalFrequencies;
+
+    internal sealed class ExportsAbstractFactory : IExportsAbstractFactory
+    {
+        public ExportsAbstractFactory()
+        {
+        }
+
+        public ISurgicalDurationExportFactory CreateSurgicalDurationExportFactory()
+        {
+            ISurgicalDurationExportFactory factory = null;
+
+            try
+            {
+                factory = new SurgicalDurationExportFactory();
+            }
+            finally
+            {
+            }
+
+            return factory;
+        }
+
+        public ISurgicalFrequencyExportFactory CreateSurgicalFrequencyExportFactory()
+        {
+            ISurgicalFrequencyExportFactory factory = null;
+
+            try
+            {
+                factory = new SurgicalFrequencyExportFactory();
+            }
+            finally
+            {
+            }
+
+            return factory;
+        }
+    }
+}
---
+++ VanHoudenhoven2007/InterfacesAbstractFactories/IExportsAbstractFactory.cs
@@ -0,0 +1,12 @@
+﻿namespace VanHoudenhoven2007.InterfacesAbstractFactories
+{
+    using VanHoudenhoven2007.InterfacesFactories.Exports.SurgicalDurations;
+    using VanHoudenhoven2007.InterfacesFactories.Exports.SurgicalFrequencies;
+
+    public interface IExportsAbstractFactory
+    {
+        ISurgicalDurationExportFactory CreateSurgicalDurationExportFactory();
+
+        ISurgicalFrequencyExportFactory CreateSurgicalFrequencyExportFactory();
+    }
+}
