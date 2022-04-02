c382352567861b69cc35f7ab4b24a65b95d511d0
Justin Britt <britt@uwindsor.ca>
2022-04-02 14:41:48 -0400

    Added factory

---
+++ VanHoudenhoven2007/Factories/Contexts/SurgicalFrequencies/SurgicalFrequencyInputContextFactory.cs
@@ -0,0 +1,34 @@
+﻿namespace VanHoudenhoven2007.Factories.Contexts.SurgicalFrequencies
+{
+    using Hl7.Fhir.Model;
+
+    using VanHoudenhoven2007.Classes.Contexts.SurgicalFrequencies;
+    using VanHoudenhoven2007.Interfaces.Contexts.SurgicalFrequencies;
+    using VanHoudenhoven2007.InterfacesFactories.Contexts.SurgicalFrequencies;
+
+    internal sealed class SurgicalFrequencyInputContextFactory : ISurgicalFrequencyInputContextFactory
+    {
+        public SurgicalFrequencyInputContextFactory()
+        {
+        }
+
+        public ISurgicalFrequencyInputContext Create(
+            PositiveInt category,
+            CodeableConcept specialty)
+        {
+            ISurgicalFrequencyInputContext context = null;
+
+            try
+            {
+                context = new SurgicalFrequencyInputContext(
+                    category,
+                    specialty);
+            }
+            finally
+            {
+            }
+
+            return context;
+        }
+    }
+}
---
+++ VanHoudenhoven2007/InterfacesFactories/Contexts/SurgicalFrequencies/ISurgicalFrequencyInputContextFactory.cs
@@ -0,0 +1,13 @@
+﻿namespace VanHoudenhoven2007.InterfacesFactories.Contexts.SurgicalFrequencies
+{
+    using Hl7.Fhir.Model;
+
+    using VanHoudenhoven2007.Interfaces.Contexts.SurgicalFrequencies;
+
+    public interface ISurgicalFrequencyInputContextFactory
+    {
+        ISurgicalFrequencyInputContext Create(
+            PositiveInt category,
+            CodeableConcept specialty);
+    }
+}
