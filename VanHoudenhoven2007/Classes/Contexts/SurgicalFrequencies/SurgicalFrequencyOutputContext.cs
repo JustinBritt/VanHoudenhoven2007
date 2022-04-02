6e75ce5a76ebf65825498621362040049ccc96eb
Justin Britt <britt@uwindsor.ca>
2022-04-02 15:21:01 -0400

    Added class

---
+++ VanHoudenhoven2007/Classes/Contexts/SurgicalFrequencies/SurgicalFrequencyOutputContext.cs
@@ -0,0 +1,17 @@
+﻿namespace VanHoudenhoven2007.Classes.Contexts.SurgicalFrequencies
+{
+    using Hl7.Fhir.Model;
+
+    using VanHoudenhoven2007.Interfaces.Contexts.SurgicalFrequencies;
+
+    internal sealed class SurgicalFrequencyOutputContext : ISurgicalFrequencyOutputContext
+    {
+        public SurgicalFrequencyOutputContext(
+            INullableValue<decimal> frequency)
+        {
+            this.Frequency = frequency;
+        }
+
+        public INullableValue<decimal> Frequency { get; }
+    }
+}
---
+++ VanHoudenhoven2007/Interfaces/Contexts/SurgicalFrequencies/ISurgicalFrequencyOutputContext.cs
@@ -0,0 +1,9 @@
+﻿namespace VanHoudenhoven2007.Interfaces.Contexts.SurgicalFrequencies
+{
+    using Hl7.Fhir.Model;
+
+    public interface ISurgicalFrequencyOutputContext
+    {
+        INullableValue<decimal> Frequency { get; }
+    }
+}
