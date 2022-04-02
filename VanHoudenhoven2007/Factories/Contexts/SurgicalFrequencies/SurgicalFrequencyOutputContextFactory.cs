25b51428ecd948ed05a730b6697388305b04f11b
Justin Britt <britt@uwindsor.ca>
2022-04-02 15:34:03 -0400

    Added factory

---
+++ VanHoudenhoven2007/Factories/Contexts/SurgicalFrequencies/SurgicalFrequencyOutputContextFactory.cs
@@ -0,0 +1,32 @@
+﻿namespace VanHoudenhoven2007.Factories.Contexts.SurgicalFrequencies
+{
+    using Hl7.Fhir.Model;
+
+    using VanHoudenhoven2007.Classes.Contexts.SurgicalFrequencies;
+    using VanHoudenhoven2007.Interfaces.Contexts.SurgicalFrequencies;
+    using VanHoudenhoven2007.InterfacesFactories.Contexts.SurgicalFrequencies;
+
+    internal sealed class SurgicalFrequencyOutputContextFactory : ISurgicalFrequencyOutputContextFactory
+    {
+        public SurgicalFrequencyOutputContextFactory()
+        {
+        }
+
+        public ISurgicalFrequencyOutputContext Create(
+            INullableValue<decimal> frequency)
+        {
+            ISurgicalFrequencyOutputContext context = null;
+
+            try
+            {
+                context = new SurgicalFrequencyOutputContext(
+                    frequency);
+            }
+            finally
+            {
+            }
+
+            return context;
+        }
+    }
+}
---
+++ VanHoudenhoven2007/InterfacesFactories/Contexts/SurgicalFrequencies/ISurgicalFrequencyOutputContextFactory.cs
@@ -0,0 +1,12 @@
+﻿namespace VanHoudenhoven2007.InterfacesFactories.Contexts.SurgicalFrequencies
+{
+    using Hl7.Fhir.Model;
+
+    using VanHoudenhoven2007.Interfaces.Contexts.SurgicalFrequencies;
+
+    public interface ISurgicalFrequencyOutputContextFactory
+    {
+        ISurgicalFrequencyOutputContext Create(
+            INullableValue<decimal> frequency);
+    }
+}
