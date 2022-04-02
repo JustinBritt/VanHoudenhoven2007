7e374429a0e1d7718c5984596bfb2aad08d8b0eb
Justin Britt <britt@uwindsor.ca>
2022-04-02 14:01:53 -0400

    Added class

---
+++ VanHoudenhoven2007/Classes/Exports/SurgicalFrequencies/SurgicalFrequencyExport.cs
@@ -0,0 +1,29 @@
+﻿namespace VanHoudenhoven2007.Classes.Exports.SurgicalFrequencies
+{
+    using Hl7.Fhir.Model;
+
+    using VanHoudenhoven2007.Extensions.SurgicalFrequencies;
+    using VanHoudenhoven2007.Interfaces.Contexts.SurgicalFrequencies;
+    using VanHoudenhoven2007.Interfaces.Exports.SurgicalFrequencies;
+    using VanHoudenhoven2007.InterfacesAbstractFactories;
+
+    public sealed class SurgicalFrequencyExport : ISurgicalFrequencyExport
+    {
+        public SurgicalFrequencyExport()
+        {
+        }
+
+        public DataType GetSurgicalFrequency(
+            IAbstractFactory abstractFactory,
+            ISurgicalFrequencyInputContext surgicalFrequencyInputContext)
+        {
+            return abstractFactory.CreateSurgicalFrequenciesAbstractFactory()
+                .CreateSurgicalFrequencyFactory(
+                    surgicalFrequencyInputContext.Specialty)
+                .Create(
+                    abstractFactory.CreateDependenciesAbstractFactory().CreateNullableValueFactory())
+                .GetValue(
+                    surgicalFrequencyInputContext.Category);
+        }
+    }
+}
---
+++ VanHoudenhoven2007/Interfaces/Exports/SurgicalFrequencies/ISurgicalFrequencyExport.cs
@@ -0,0 +1,14 @@
+﻿namespace VanHoudenhoven2007.Interfaces.Exports.SurgicalFrequencies
+{
+    using Hl7.Fhir.Model;
+
+    using VanHoudenhoven2007.Interfaces.Contexts.SurgicalFrequencies;
+    using VanHoudenhoven2007.InterfacesAbstractFactories;
+
+    public interface ISurgicalFrequencyExport
+    {
+        DataType GetSurgicalFrequency(
+            IAbstractFactory abstractFactory,
+            ISurgicalFrequencyInputContext surgicalFrequencyInputContext);
+    }
+}
