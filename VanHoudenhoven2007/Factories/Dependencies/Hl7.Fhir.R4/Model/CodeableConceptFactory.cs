d20af4f279ef8fedf1e86d8337dd1d8df2215362
Justin Britt <britt@uwindsor.ca>
2022-04-02 16:13:23 -0400

    Added factory

---
+++ VanHoudenhoven2007/Factories/Dependencies/Hl7.Fhir.R4/Model/CodeableConceptFactory.cs
@@ -0,0 +1,34 @@
+﻿namespace VanHoudenhoven2007.Factories.Dependencies.Hl7.Fhir.R4.Model
+{
+    using global::Hl7.Fhir.Model;
+
+    using VanHoudenhoven2007.InterfacesFactories.Dependencies.Hl7.Fhir.R4.Model;
+
+    internal sealed class CodeableConceptFactory : ICodeableConceptFactory
+    {
+        public CodeableConceptFactory()
+        {
+        }
+
+        public CodeableConcept Create(
+            string code,
+            string system,
+            string text)
+        {
+            CodeableConcept codeableConcept;
+
+            try
+            {
+                codeableConcept = new CodeableConcept(
+                    system: system,
+                    code: code,
+                    text: text);
+            }
+            finally
+            {
+            }
+
+            return codeableConcept;
+        }
+    }
+}
---
+++ VanHoudenhoven2007/InterfacesFactories/Dependencies/Hl7.Fhir.R4/Model/ICodeableConceptFactory.cs
@@ -0,0 +1,10 @@
+﻿namespace VanHoudenhoven2007.InterfacesFactories.Dependencies.Hl7.Fhir.R4.Model
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public interface ICodeableConceptFactory
+    {
+    }
+}
