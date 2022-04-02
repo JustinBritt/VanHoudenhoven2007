d0d34af47d4db0ac54be5097f4cc0a1ed50f8539
Justin Britt <britt@uwindsor.ca>
2022-04-02 11:27:55 -0400

    Added class

---
+++ VanHoudenhoven2007/Extensions/SurgicalFrequencies/SurgicalFrequencyExtensions.cs
@@ -0,0 +1,35 @@
+﻿namespace VanHoudenhoven2007.Extensions.SurgicalFrequencies
+{
+    using Hl7.Fhir.Model;
+
+    using VanHoudenhoven2007.Interfaces.SurgicalFrequencies;
+
+    public static class SurgicalFrequencyExtensions
+    {
+        public static DataType GetValue(
+            this ISurgicalFrequency surgicalFrequency,
+            PositiveInt category)
+        {
+            return category.Value switch
+            {
+                1 => surgicalFrequency.Category1Frequency,
+
+                2 => surgicalFrequency.Category2Frequency,
+
+                3 => surgicalFrequency.Category3Frequency,
+
+                4 => surgicalFrequency.Category4Frequency,
+
+                5 => surgicalFrequency.Category5Frequency,
+
+                6 => surgicalFrequency.Category6Frequency,
+
+                7 => surgicalFrequency.Category7Frequency,
+
+                8 => surgicalFrequency.Category8Frequency,
+
+                _ => null
+            };
+        }
+    }
+}
