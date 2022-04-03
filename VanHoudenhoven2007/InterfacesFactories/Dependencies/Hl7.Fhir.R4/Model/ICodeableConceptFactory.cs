83343f373b33813e997c4a57afc8fd65e52f7fdb
Justin Britt <britt@uwindsor.ca>
2022-04-02 20:49:11 -0400

    Added methods

--- VanHoudenhoven2007/InterfacesFactories/Dependencies/Hl7.Fhir.R4/Model/ICodeableConceptFactory.cs
+++ VanHoudenhoven2007/InterfacesFactories/Dependencies/Hl7.Fhir.R4/Model/ICodeableConceptFactory.cs
@@ -1,10 +1,34 @@
 ﻿namespace VanHoudenhoven2007.InterfacesFactories.Dependencies.Hl7.Fhir.R4.Model
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Text;
+    using global::Hl7.Fhir.Model;
 
     public interface ICodeableConceptFactory
     {
+        CodeableConcept Create(
+            string code,
+            string system,
+            string text = null);
+
+        CodeableConcept CreateEarNoseThroatSurgery();
+
+        CodeableConcept CreateGeneralSurgery();
+
+        CodeableConcept CreateGynecologicalSurgery();
+
+        CodeableConcept CreateNeurosurgery();
+
+        CodeableConcept CreateOphthalmicSurgery();
+
+        CodeableConcept CreateOphthalmology();
+
+        CodeableConcept CreateOralSurgery();
+
+        CodeableConcept CreateOrthopedicSurgery();
+
+        CodeableConcept CreatePlasticSurgery();
+
+        CodeableConcept CreateTrauma();
+
+        CodeableConcept CreateUrology();
     }
 }
