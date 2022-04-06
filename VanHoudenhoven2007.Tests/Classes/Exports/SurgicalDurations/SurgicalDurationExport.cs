682838aefac861495b01da342910edfd01d67983
Justin Britt <britt@uwindsor.ca>
2022-04-06 03:17:33 -0400

    Added tests

---
+++ VanHoudenhoven2007.Tests/Classes/Exports/SurgicalDurations/SurgicalDurationExport.cs
@@ -0,0 +1,13 @@
+namespace VanHoudenhoven2007.Tests.Classes.Exports.SurgicalDurations
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public sealed class SurgicalDurationExport
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ VanHoudenhoven2007.Tests/Classes/Exports/SurgicalFrequencies/SurgicalFrequencyExport.cs
@@ -0,0 +1,13 @@
+﻿namespace VanHoudenhoven2007.Tests.Classes.Exports.SurgicalFrequencies
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public sealed class SurgicalFrequencyExport
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
