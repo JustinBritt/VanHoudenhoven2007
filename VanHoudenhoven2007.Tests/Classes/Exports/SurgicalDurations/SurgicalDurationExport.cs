ee494d461872be30e8d5ffeca1160dd0f9672646
Justin Britt <britt@uwindsor.ca>
2022-04-06 03:29:08 -0400

    Changes to usings

--- VanHoudenhoven2007.Tests/Classes/Exports/SurgicalDurations/SurgicalDurationExport.cs
+++ VanHoudenhoven2007.Tests/Classes/Exports/SurgicalDurations/SurgicalDurationExport.cs
@@ -1,6 +1,13 @@
 namespace VanHoudenhoven2007.Tests.Classes.Exports.SurgicalDurations
 {
+    using System;
+
     using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    using VanHoudenhoven2007.AbstractFactories;
+    using VanHoudenhoven2007.InterfacesAbstractFactories;
+    using VanHoudenhoven2007.Interfaces.Contexts.SurgicalDurations;
+    using VanHoudenhoven2007.Interfaces.Exports.SurgicalDurations;
 
     [TestClass]
     public sealed class SurgicalDurationExport
