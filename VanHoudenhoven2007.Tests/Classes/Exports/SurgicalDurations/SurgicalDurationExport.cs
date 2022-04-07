6dc5858cc35b980ae852c61d0b593235f440d85a
Justin Britt <britt@uwindsor.ca>
2022-04-06 23:31:24 -0400

    Added test

--- VanHoudenhoven2007.Tests/Classes/Exports/SurgicalDurations/SurgicalDurationExport.cs
+++ VanHoudenhoven2007.Tests/Classes/Exports/SurgicalDurations/SurgicalDurationExport.cs
@@ -45,5 +45,35 @@ namespace VanHoudenhoven2007.Tests.Classes.Exports.SurgicalDurations
                 expected: minutes,
                 actual: surgicalDurationOutputContext.Duration.UnitElement.Value);
         }
+
+        [TestMethod]
+        public void Category2EarNoseThroatSurgeryAverage()
+        {
+            // Arrange
+            IAbstractFactory abstractFactory = AbstractFactory.Create();
+
+            IDependenciesAbstractFactory dependenciesAbstractFactory = abstractFactory.CreateDependenciesAbstractFactory();
+
+            ISurgicalDurationInputContext surgicalDurationInputContext = abstractFactory.CreateContextsAbstractFactory().CreateSurgicalDurationInputContextFactory().Create(
+                category: dependenciesAbstractFactory.CreateNullableValueFactory().Create<int>(2),
+                specialty: dependenciesAbstractFactory.CreateCodeableConceptFactory().CreateEarNoseThroatSurgery(),
+                statistic: dependenciesAbstractFactory.CreateValueFactory().CreateAverage());
+
+            ISurgicalDurationExport surgicalDurationExport = abstractFactory.CreateExportsAbstractFactory().CreateSurgicalDurationExportFactory().Create();
+
+            // Act
+            ISurgicalDurationOutputContext surgicalDurationOutputContext = surgicalDurationExport.GetSurgicalDuration(
+                abstractFactory,
+                surgicalDurationInputContext);
+
+            // Assert
+            Assert.AreEqual(
+                expected: 40m,
+                actual: surgicalDurationOutputContext.Duration.Value.Value);
+
+            Assert.AreEqual(
+                expected: minutes,
+                actual: surgicalDurationOutputContext.Duration.UnitElement.Value);
+        }
     }
 }
