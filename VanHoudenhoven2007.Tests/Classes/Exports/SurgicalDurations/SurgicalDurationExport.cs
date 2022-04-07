3e35619d2f4ee089dac9e1e2286b1e722dc0b6bf
Justin Britt <britt@uwindsor.ca>
2022-04-07 00:45:17 -0400

    Added builder class

--- VanHoudenhoven2007.Tests/Classes/Exports/SurgicalDurations/SurgicalDurationExport.cs
+++ VanHoudenhoven2007.Tests/Classes/Exports/SurgicalDurations/SurgicalDurationExport.cs
@@ -9,6 +9,91 @@ namespace VanHoudenhoven2007.Tests.Classes.Exports.SurgicalDurations
     using VanHoudenhoven2007.Interfaces.Contexts.SurgicalDurations;
     using VanHoudenhoven2007.Interfaces.Exports.SurgicalDurations;
 
+    public interface ISurgicalDurationExportTestBuilder
+    {
+        ISurgicalDurationInputContext SurgicalDurationInputContext { get; }
+
+        ISurgicalDurationExport SurgicalDurationExport { get; }
+
+        ISurgicalDurationOutputContext SurgicalDurationOutputContext { get; }
+
+        void Build();
+
+        void WithAverage();
+
+        void WithCategory(
+            int category);
+
+        void WithEarNoseThroat();
+    }
+
+    public sealed class SurgicalDurationExportTestBuilder : ISurgicalDurationExportTestBuilder
+    {
+        public SurgicalDurationExportTestBuilder()
+        {
+            this.AbstractFactory = VanHoudenhoven2007.AbstractFactories.AbstractFactory.Create();
+
+            this.DependenciesAbstractFactory = this.AbstractFactory.CreateDependenciesAbstractFactory();
+        }
+
+        public IAbstractFactory AbstractFactory { get; }
+
+        public Hl7.Fhir.Model.INullableValue<int> Category { get; private set; }
+
+        public IDependenciesAbstractFactory DependenciesAbstractFactory { get; }
+
+        public Hl7.Fhir.Model.CodeableConcept Specialty { get; private set; }
+
+        public Hl7.Fhir.Model.IValue<string> Statistic { get; private set; }
+
+        public ISurgicalDurationInputContext SurgicalDurationInputContext { get; private set; }
+
+        public ISurgicalDurationExport SurgicalDurationExport { get; private set; }
+
+        public ISurgicalDurationOutputContext SurgicalDurationOutputContext { get; private set; }
+
+        public void Build(
+            Hl7.Fhir.Model.INullableValue<int> category,
+            Hl7.Fhir.Model.CodeableConcept specialty,
+            Hl7.Fhir.Model.IValue<string> statistic)
+        {
+            this.SurgicalDurationInputContext = this.AbstractFactory.CreateContextsAbstractFactory().CreateSurgicalDurationInputContextFactory().Create(
+                category: category,
+                specialty: specialty,
+                statistic: statistic);
+
+            this.SurgicalDurationExport = this.AbstractFactory.CreateExportsAbstractFactory().CreateSurgicalDurationExportFactory().Create();
+
+            this.SurgicalDurationOutputContext = this.SurgicalDurationExport.GetSurgicalDuration(
+                this.AbstractFactory,
+                this.SurgicalDurationInputContext);
+        }
+
+        public void Build()
+        {
+            this.Build(
+                category: this.Category,
+                specialty: this.Specialty,
+                statistic: this.Statistic);
+        }
+
+        public void WithAverage()
+        {
+            this.Statistic = this.DependenciesAbstractFactory.CreateValueFactory().CreateAverage();
+        }
+
+        public void WithCategory(
+            int category)
+        {
+            this.Category = this.DependenciesAbstractFactory.CreateNullableValueFactory().Create<int>(category);
+        }
+
+        public void WithEarNoseThroat()
+        {
+            this.Specialty = this.DependenciesAbstractFactory.CreateCodeableConceptFactory().CreateEarNoseThroatSurgery();
+        }
+    }
+
     [TestClass]
     public sealed class SurgicalDurationExport
     {
@@ -20,90 +105,69 @@ namespace VanHoudenhoven2007.Tests.Classes.Exports.SurgicalDurations
         public void Category1EarNoseThroatSurgeryAverage()
         {
             // Arrange
-            IAbstractFactory abstractFactory = AbstractFactory.Create();
-
-            IDependenciesAbstractFactory dependenciesAbstractFactory = abstractFactory.CreateDependenciesAbstractFactory();
-
-            ISurgicalDurationInputContext surgicalDurationInputContext = abstractFactory.CreateContextsAbstractFactory().CreateSurgicalDurationInputContextFactory().Create(
-                category: dependenciesAbstractFactory.CreateNullableValueFactory().Create<int>(1),
-                specialty: dependenciesAbstractFactory.CreateCodeableConceptFactory().CreateEarNoseThroatSurgery(),
-                statistic: dependenciesAbstractFactory.CreateValueFactory().CreateAverage());
-
-            ISurgicalDurationExport surgicalDurationExport = abstractFactory.CreateExportsAbstractFactory().CreateSurgicalDurationExportFactory().Create();
+            ISurgicalDurationExportTestBuilder builder = new SurgicalDurationExportTestBuilder();
 
             // Act
-            ISurgicalDurationOutputContext surgicalDurationOutputContext = surgicalDurationExport.GetSurgicalDuration(
-                abstractFactory,
-                surgicalDurationInputContext);
+            builder.WithCategory(
+                category: 1);
+            builder.WithEarNoseThroat();
+            builder.WithAverage();
+            builder.Build();
 
             // Assert
             Assert.AreEqual(
                 expected: 102m,
-                actual: surgicalDurationOutputContext.Duration.Value.Value);
+                actual: builder.SurgicalDurationOutputContext.Duration.Value.Value);
 
             Assert.AreEqual(
                 expected: minutes,
-                actual: surgicalDurationOutputContext.Duration.UnitElement.Value);
+                actual: builder.SurgicalDurationOutputContext.Duration.UnitElement.Value);
         }
 
         [TestMethod]
         public void Category2EarNoseThroatSurgeryAverage()
         {
             // Arrange
-            IAbstractFactory abstractFactory = AbstractFactory.Create();
-
-            IDependenciesAbstractFactory dependenciesAbstractFactory = abstractFactory.CreateDependenciesAbstractFactory();
-
-            ISurgicalDurationInputContext surgicalDurationInputContext = abstractFactory.CreateContextsAbstractFactory().CreateSurgicalDurationInputContextFactory().Create(
-                category: dependenciesAbstractFactory.CreateNullableValueFactory().Create<int>(2),
-                specialty: dependenciesAbstractFactory.CreateCodeableConceptFactory().CreateEarNoseThroatSurgery(),
-                statistic: dependenciesAbstractFactory.CreateValueFactory().CreateAverage());
-
-            ISurgicalDurationExport surgicalDurationExport = abstractFactory.CreateExportsAbstractFactory().CreateSurgicalDurationExportFactory().Create();
+            SurgicalDurationExportTestBuilder builder = new SurgicalDurationExportTestBuilder();
 
             // Act
-            ISurgicalDurationOutputContext surgicalDurationOutputContext = surgicalDurationExport.GetSurgicalDuration(
-                abstractFactory,
-                surgicalDurationInputContext);
+            builder.WithCategory(
+                category: 2);
+            builder.WithEarNoseThroat();
+            builder.WithAverage();
+            builder.Build();
 
             // Assert
             Assert.AreEqual(
                 expected: 40m,
-                actual: surgicalDurationOutputContext.Duration.Value.Value);
+                actual: builder.SurgicalDurationOutputContext.Duration.Value.Value);
 
             Assert.AreEqual(
                 expected: minutes,
-                actual: surgicalDurationOutputContext.Duration.UnitElement.Value);
+                actual: builder.SurgicalDurationOutputContext.Duration.UnitElement.Value);
         }
 
         [TestMethod]
         public void Category3EarNoseThroatSurgeryAverage()
         {
             // Arrange
-            IAbstractFactory abstractFactory = AbstractFactory.Create();
-
-            IDependenciesAbstractFactory dependenciesAbstractFactory = abstractFactory.CreateDependenciesAbstractFactory();
-
-            ISurgicalDurationInputContext surgicalDurationInputContext = abstractFactory.CreateContextsAbstractFactory().CreateSurgicalDurationInputContextFactory().Create(
-                category: dependenciesAbstractFactory.CreateNullableValueFactory().Create<int>(3),
-                specialty: dependenciesAbstractFactory.CreateCodeableConceptFactory().CreateEarNoseThroatSurgery(),
-                statistic: dependenciesAbstractFactory.CreateValueFactory().CreateAverage());
-
-            ISurgicalDurationExport surgicalDurationExport = abstractFactory.CreateExportsAbstractFactory().CreateSurgicalDurationExportFactory().Create();
+            SurgicalDurationExportTestBuilder builder = new SurgicalDurationExportTestBuilder();
 
             // Act
-            ISurgicalDurationOutputContext surgicalDurationOutputContext = surgicalDurationExport.GetSurgicalDuration(
-                abstractFactory,
-                surgicalDurationInputContext);
+            builder.WithCategory(
+                category: 3);
+            builder.WithEarNoseThroat();
+            builder.WithAverage();
+            builder.Build();
 
             // Assert
             Assert.AreEqual(
                 expected: 65m,
-                actual: surgicalDurationOutputContext.Duration.Value.Value);
+                actual: builder.SurgicalDurationOutputContext.Duration.Value.Value);
 
             Assert.AreEqual(
                 expected: minutes,
-                actual: surgicalDurationOutputContext.Duration.UnitElement.Value);
+                actual: builder.SurgicalDurationOutputContext.Duration.UnitElement.Value);
         }
     }
 }
