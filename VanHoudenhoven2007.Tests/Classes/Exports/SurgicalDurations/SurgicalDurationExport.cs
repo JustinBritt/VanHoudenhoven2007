9903144f41aff2ef0c82c67cd01360478b8c95ac
Justin Britt <britt@uwindsor.ca>
2022-04-09 14:54:20 -0400

    Changes to usings

--- VanHoudenhoven2007.Tests/Classes/Exports/SurgicalDurations/SurgicalDurationExport.cs
+++ VanHoudenhoven2007.Tests/Classes/Exports/SurgicalDurations/SurgicalDurationExport.cs
@@ -1,8 +1,6 @@
 namespace VanHoudenhoven2007.Tests.Classes.Exports.SurgicalDurations
 {
-    using System;
     using System.Collections.Generic;
-    using System.Linq;
 
     using Microsoft.VisualStudio.TestTools.UnitTesting;
 
