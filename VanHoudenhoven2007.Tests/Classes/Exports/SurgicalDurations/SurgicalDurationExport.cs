506af2d356f656b37c05f0c3df24e3ea8297bf06
Justin Britt <britt@uwindsor.ca>
2022-04-09 05:34:50 -0400

    Use DynamicData

--- VanHoudenhoven2007.Tests/Classes/Exports/SurgicalDurations/SurgicalDurationExport.cs
+++ VanHoudenhoven2007.Tests/Classes/Exports/SurgicalDurations/SurgicalDurationExport.cs
@@ -277,6 +277,19 @@ namespace VanHoudenhoven2007.Tests.Classes.Exports.SurgicalDurations
                 new object[] { 8, 0m, minutes },
             };
 
+        private static IEnumerable<object[]> NeurosurgeryAverageData =>
+            new[]
+            {
+                new object[] { 1, 192m, minutes },
+                new object[] { 2, 113m, minutes },
+                new object[] { 3, 171m, minutes },
+                new object[] { 4, 255m, minutes },
+                new object[] { 5, 324m, minutes },
+                new object[] { 6, 492m, minutes },
+                new object[] { 7, 0m, minutes },
+                new object[] { 8, 0m, minutes },
+            };
+
         [TestMethod]
         [DynamicData(nameof(EarNoseThroatSurgeryAverageData))]
         public void EarNoseThroatSurgeryAverage(
@@ -446,42 +459,31 @@ namespace VanHoudenhoven2007.Tests.Classes.Exports.SurgicalDurations
         }
 
         [TestMethod]
-        public void NeurosurgeryAverage()
+        [DynamicData(nameof(NeurosurgeryAverageData))]
+        public void NeurosurgeryAverage(
+            int category,
+            decimal value,
+            string unit)
         {
             // Arrange
             ISurgicalDurationExportTestBuilder builder = new SurgicalDurationExportTestBuilder();
 
-            int numberCategories = 8;
+            // Act
+            builder
+                .WithCategory(
+                    category: category)
+                .WithNeurosurgery()
+                .WithAverage()
+                .Build();
 
-            Span<decimal> data = (Span<decimal>)Array.CreateInstance(typeof(decimal), numberCategories + 1);
-            data[1] = 192m;
-            data[2] = 113m;
-            data[3] = 171m;
-            data[4] = 255m;
-            data[5] = 324m;
-            data[6] = 492m;
-            data[7] = 0m;
-            data[8] = 0m;
+            // Assert
+            Assert.AreEqual(
+                expected: value,
+                actual: builder.SurgicalDurationOutputContext.Duration.Value.Value);
 
-            for (int i = 1; i <= numberCategories; i = i + 1)
-            {
-                // Act
-                builder
-                     .WithCategory(
-                         category: i)
-                     .WithNeurosurgery()
-                     .WithAverage()
-                     .Build();
-
-                // Assert
-                Assert.AreEqual(
-                    expected: data[i],
-                    actual: builder.SurgicalDurationOutputContext.Duration.Value.Value);
-
-                Assert.AreEqual(
-                    expected: minutes,
-                    actual: builder.SurgicalDurationOutputContext.Duration.UnitElement.Value);
-            }
+            Assert.AreEqual(
+                expected: unit,
+                actual: builder.SurgicalDurationOutputContext.Duration.UnitElement.Value);
         }
 
         [TestMethod]
